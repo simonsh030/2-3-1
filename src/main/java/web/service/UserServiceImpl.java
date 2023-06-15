62cb4b64646730bc924cc120e09730f73e061487
simonsh030 <icesert75@gmail.com>
2023-06-15 14:40:54 +0300

    .

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User updatedUser) {
+        userDao.updateUser(updatedUser);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUserById(int id) {
+        userDao.deleteUserById(id);
+    }
+}
---
+++ src/webapp/index.jsp
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Active Link Button</title>
+    <style>
+        body {
+            background-color: #FFFFFF;
+        }
+
+
+
+        .button {
+            display: inline-block;
+            background-color: #0078E7;
+            border: none;
+            color: #FFFFFF;
+            padding: 30px 60px;
+            text-align: center;
+            text-decoration: none;
+            font-size: 48px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 8px;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr>
+        <td>
+            <a href="http://localhost:8080/PP_2_3_1_CRUD_war_exploded/users" class="button">Go to user list</a>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
